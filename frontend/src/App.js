3c831cdd22805d2f6cb380f852c97bdf9f91f925
TonyyyJ <tonyyyjin412@gmail.com>
2023-06-13 01:05:55 -0400

    add search bar and fix css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,41 @@
-
+import React, { useState } from 'react';
+import './App.css';
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState('');
   const image = {
     iamgeUrl: 'https://yt3.googleusercontent.com/584JjRp5QMuKbyduM_2k5RlXFqHJtQ0qLIPZpwbUjMJmgzZngHcam5JMuZQxyzGMV5ljwJRl0Q=s176-c-k-c0x00ffffff-no-rj',
     imageSize: 220,
   }
   
+  const search = (e) => {
+    e.preventDefault();
+    console.log('Search term:', searchTerm);
+  };
+
+
   return (
-    <div className="App">
-      <h1>Welcome to my app</h1>
+    <div>
+   
+      <h1 >YouTubeRs</h1>
+    
       <img
         src = {image.iamgeUrl}
         style={{
             width: image.imageSize,
             height: image.imageSize
          }}
-         
-      />
+
+      ></img>
+      <from onsubmit = {search}>
+        <input 
+            type = "text"
+            value = {searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            placeholder="Search..."
+        ></input>
+        <button type="submit">Search</button>
+      </from>
     </div>
   );
 }
