64ec29d97c098c8158a04b7ee9c55efcf67964f1
TonyyyJ <tonyyyjin412@gmail.com>
2023-06-12 23:31:08 -0400

    add image

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,22 @@
 
 
 function App() {
+  const image = {
+    iamgeUrl: 'https://yt3.googleusercontent.com/584JjRp5QMuKbyduM_2k5RlXFqHJtQ0qLIPZpwbUjMJmgzZngHcam5JMuZQxyzGMV5ljwJRl0Q=s176-c-k-c0x00ffffff-no-rj',
+    imageSize: 220,
+  }
+  
   return (
     <div className="App">
       <h1>Welcome to my app</h1>
+      <img
+        src = {image.iamgeUrl}
+        style={{
+            width: image.imageSize,
+            height: image.imageSize
+         }}
+         
+      />
     </div>
   );
 }
