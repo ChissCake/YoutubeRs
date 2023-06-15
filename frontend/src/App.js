88141c73962832fcc1dbe870f3c955362490d6b5
Henry Cao <cao.1042>
2023-06-14 21:44:06 -0400

    added navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,18 @@
 import React, { useState } from 'react';
 import './App.css';
+import Navbar from './components';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages';
+import About from './pages/about';
+import Events from './pages/events';
+import Search from './pages/search';
+import SignUp from './pages/signup';
 
 function App() {
   const [searchTerm, setSearchTerm] = useState('');
   const image = {
     iamgeUrl: 'https://yt3.googleusercontent.com/584JjRp5QMuKbyduM_2k5RlXFqHJtQ0qLIPZpwbUjMJmgzZngHcam5JMuZQxyzGMV5ljwJRl0Q=s176-c-k-c0x00ffffff-no-rj',
-    imageSize: 220,
+    imageSize: 100,
   }
   
   const search = (e) => {
@@ -16,8 +23,17 @@ function App() {
 
   return (
     <div>
-   
-      <h1 >YouTubeRs</h1>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' exact component={Home} />
+          <Route path='/about' component={About} />
+          <Route path='/events' component={Events} />
+          <Route path='/search' component={Search} />
+          <Route path='/sign-up' component={SignUp} />
+        </Routes>
+      </Router>
+      <h1 >YourTubeRs</h1>
     
       <img
         src = {image.iamgeUrl}
