19266a6c1a83980942e03ab979caf9020ae24598
Henry Cao <cao.1042>
2023-07-06 00:28:48 -0400

    added responsive search button page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Routes, Route, useHistory } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useNavigate, BrowserRouter } from 'react-router-dom';
 import Home from './pages';
 import Result from './pages/result';
 import About from './pages/about';
@@ -9,27 +9,29 @@ import Events from './pages/events';
 import SignUp from './pages/signup';
 
 function App() {
-  const [searchTerm, setSearchTerm] = useState('');
   const image = {
     iamgeUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/YouTube_full-color_icon_%282017%29.svg/159px-YouTube_full-color_icon_%282017%29.svg.png?2021101507481',
     imageSize: 100,
     width: 120,
   };
 
+  const history = useNavigate();
+  const [searchTerm, setSearchTerm] = useState('');
   const search = (e) => {
     e.preventDefault();
     console.log('Search term:', searchTerm);
   };
 
   return (
-    <Router>
-    <div>
+    
+      <div>
       
         <Navbar />
         <Routes>
           <Route path='/about' element={<About />} />
           <Route path='/events' element={<Events />} />
           <Route path='/sign-up' element={<SignUp />} />
+          <Route path="/result" element={<Result searchTerm={searchTerm}/>} />
         </Routes>
          
 
@@ -42,20 +44,19 @@ function App() {
                 width: image.width,
                 height: image.imageSize
             }}
-            alt="YouTube Logo"
           ></img>
-          <form onsubmit = {search}>
+          <from onsubmit = {search}>
             <input 
               type = "text"
               value = {searchTerm}
               onChange={(e) => setSearchTerm(e.target.value)}
               placeholder="Search..."
             ></input>
-            <button type="submit">Search</button>
-          </form>
+            <button onClick={()=> history("/result")}>Search</button>
+          </from>
         </div>
       </div>
-    </Router>
+    
   );
 }
 
