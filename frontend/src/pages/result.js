910e06db37be78b0ac21d8136f1ccb21ac328388
Henry Cao <cao.1042>
2023-07-25 15:08:42 -0400

    Added a section displaying top 5 search channels

--- frontend/src/pages/result.js
+++ frontend/src/pages/result.js
@@ -1,22 +1,35 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import { useLocation } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Result = () => {
-  // Use the useParams hook to access the searchTerm from the URL
-  const { searchTerm } = useParams();
+  const [searchResults, setSearchResults] = useState([]);
+
+  useEffect(() => {
+    const fetchSearchResults = async () => {
+      try {
+        const apiKey = 'YOUR_YOUTUBE_API_KEY';
+        const response = await axios.get(
+          `https://www.googleapis.com/youtube/v3/search?part=snippet&q=YOUR_SEARCH_QUERY&type=video&key=${apiKey}&maxResults=5`
+        );
+        setSearchResults(response.data.items);
+      } catch (error) {
+        console.error('Error fetching search results:', error);
+      }
+    };
+
+    fetchSearchResults();
+  }, []);
+
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '100vh',
-      }}
-    >
-      <h1>
-        Top 5 Search Result For: {searchTerm}
-      </h1>
+    <div>
+      <h1>Top 5 Search Results:</h1>
+      {searchResults.map((result) => (
+        <div key={result.id.videoId}>
+          <img src={result.snippet.thumbnails.default.url} alt={result.snippet.title} />
+          <h3>{result.snippet.title}</h3>
+          <p>{result.snippet.channelTitle}</p>
+        </div>
+      ))}
     </div>
   );
 };
